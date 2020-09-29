3eadfcef2185156513302c26cd92f3ae377352e3
orest <amx1715gmail@.com>
2020-09-30 00:16:33 +0300

    test commit

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Starter on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
