b9983ab43657624f218c7ff94f38afc7cbc45972
orest <amx1715gmail@.com>
2020-09-30 20:12:49 +0300

    30-09

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+const User = require("../../models/User");
 // @ Route GET api/users
 
 router.post(
@@ -13,14 +14,32 @@ router.post(
       min: 4,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+      res.send("User route");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
